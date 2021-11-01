a47bc878acba76f724f84ac214d5ceb442f582c0
ack61 <85378035+ack61@users.noreply.github.com>
2021-11-01 23:50:01 +0900

    static呼び出しを減らした

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import mediator.Engine;
+import mediator.WordsAnalyzer;
 
 public class Main{
 	public static void main(String args[]){
@@ -14,5 +15,11 @@ public class Main{
 		Engine engine = new Engine();
 		Object result = engine.eval(script);
 		System.out.println(result);
+		
+		WordsAnalyzer wa = new WordsAnalyzer();
+		String words[] = wa.analyzeFromFileName("test.medi");
+		for(int i = 0; i < words.length; i++){
+			System.out.println(words[i]);
+		}
 	}
 }
