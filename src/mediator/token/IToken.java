90ef80249a5812da05719bff0db8b35d3bd287f6
ack61 <85378035+ack61@users.noreply.github.com>
2021-11-01 23:55:28 +0900

    定数名の変更

--- src/mediator/token/IToken.java
+++ src/mediator/token/IToken.java
@@ -5,7 +5,7 @@ public interface IToken{
 	public static final int TOKEN_STRING  = 0;	//シングル、ダブルクォーテーション文字
 	public static final int TOKEN_NUMBER  = 1;	//数字
 	public static final int TOKEN_SYMBOL  = 2;	//記号
-	public static final int TOKEN_KEYWORD = 3;	//予約語、変数名、型名、etc.
+	public static final int TOKEN_WORD    = 3;	//予約語、変数名、型名、etc.
 	
 	public String get();
 	public int getType();
