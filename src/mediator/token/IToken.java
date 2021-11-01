29b5e4b7460cc86f7751341ef15eb115efbe77e5
ack61 <85378035+ack61@users.noreply.github.com>
2021-11-01 23:45:15 +0900

    字句解析のトークンのインターフェースの作成

---
+++ src/mediator/token/IToken.java
@@ -0,0 +1,12 @@
+package mediator.token;
+
+public interface IToken{
+	public static final int TOKEN_COMMENT = -1;	//コメント
+	public static final int TOKEN_STRING  = 0;	//シングル、ダブルクォーテーション文字
+	public static final int TOKEN_NUMBER  = 1;	//数字
+	public static final int TOKEN_SYMBOL  = 2;	//記号
+	public static final int TOKEN_KEYWORD = 3;	//予約語、変数名、型名、etc.
+	
+	public String get();
+	public int getType();
+}
