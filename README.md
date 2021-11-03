9e4e0ed8e2ed6a62931b4c3c5754abfbf4b4f65b
ack61 <85378035+ack61@users.noreply.github.com>
2021-11-04 00:37:33 +0900

    エンジンの削除
    エンジンを消した
    それに伴ってMainとコンパイルコマンドが変更
    一応仮置き場に残しておく

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### コンパイルコマンド
 src下で、
 
-`javac -encoding UTF-8 *.java mediator\*.java mediator\expression\*.java mediator\operator\*.java mediator\token\*.java`
+`javac -encoding UTF-8 *.java mediator\*.java mediator\token\*.java`
 ### 実行コマンド
 src下で、
 
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,10 @@
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
-import mediator.Engine;
 import mediator.token.IToken;
 import mediator.WordsAnalyzer;
 
 public class Main{
 	public static void main(String args[]){
-		List<Object> script = new ArrayList<Object>();
-		script.add("+");
-		script.add(new BigDecimal("1"));
-		script.add(new BigDecimal("9"));
-		
-		Engine engine = new Engine();
-		Object result = engine.eval(script);
-		System.out.println(result);
-		
 		WordsAnalyzer wa = new WordsAnalyzer();
-		IToken words[] = wa.analyzeFromFileName("test.medi");
+		IToken words[] = wa.analyzeFromFileName("Sample.medi");
 		for(int i = 0; i < words.length; i++){
 			System.out.println(words[i].get());
 		}
